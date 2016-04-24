e662111e9efbf3b3ca1d0811031a8f060e97666c
Evan Cofer <ecofer@trinity.edu>
2016-04-24 16:57:59 -0500

    Added some templates and index controller

--- server/app/controllers/ApplicationController.scala
+++ server/app/controllers/ApplicationController.scala
@@ -11,7 +11,32 @@ import play.api.libs.json.JsValue
 import play.api.libs.json.JsString
 import scala.concurrent.Await
 
-class ApplicationController extends Controller { 
+import services.UserService
+import services.LinkService
 
+class ApplicationController extends Controller {
+  
+  def index = Action.async { implicit request =>
+    {
+      println("index")
+      request.session.get("user") match {
+        case None => Future { Ok(views.html.index(Forms.userForm)) }
+        case Some(userName) => {
+          UserService.getUserByName(userName).map {
+            _ match {
+              case Some(user) => {
+                println("Found username while loading index:" + userName)
+                links_ => Ok(views.html.links).withSession("user" -> user.name)
+              }
+              case None => {
+                println("No username found for" + userName)
+                Ok(views.html.index(Forms.userForm))
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
 }
--- server/app/services/UserService.scala
+++ server/app/services/UserService.scala
@@ -8,5 +8,5 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object UserService {
 //Implement UserService like a wrapper for all user actions so that controllers has no dependencies on the implementations such as DB access/registration, and so we can also separate controller from any dependencies on OAuth for reddit account usage.
 
-
+  def getUserByName(userName:String):User = {"yo"}
 }
