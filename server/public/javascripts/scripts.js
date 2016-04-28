357aca64b645816a8a3207efdf4a71a1ef3c3d07
Evan Cofer <ecofer@trinity.edu>
2016-04-28 17:46:52 -0500

    Did initial styling of login page
    Cant figure out how to color the login buttons well in an efficient manner due to the nature of forms

--- server/public/javascripts/scripts.js
+++ server/public/javascripts/scripts.js
@@ -13,3 +13,18 @@ function logout(){
 		}
 	});
 }
+
+function js_inputButton(button){
+	console.log(button);
+	if ($("#username").val() && $('#password').val()){
+		button.className = "btn btn-block btn-success"
+	} else {
+		button.className = "btn btn-block disabled"
+	}
+}
+
+function js_register(){
+	$('#register-modal').modal();
+	$('#register-modal').modal("show");
+			
+}
