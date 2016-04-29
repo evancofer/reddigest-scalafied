cfcce1e45505b924b43395493f55667acc0ee4de
Evan Cofer <ecofer@trinity.edu>
2016-04-29 16:15:36 -0500

    Activated the getLinks route and change logout from ajax to full page refresh.

--- server/public/javascripts/scripts.js
+++ server/public/javascripts/scripts.js
@@ -1,19 +1,3 @@
-/* 
- * Not worth doing this in scalajs and having to worry
- * about some poorly documented or unimplemented 
- * feature, esp. when if we start using OAuth.
- */
-function logout(){
-	$.ajax({
-		url: "/logout",
-		method: "POST",
-		dataType: "json",
-		success:function(data){
-			location.reload();
-		}
-	});
-}
-
 function js_inputButton(button){
 	console.log(button);
 	if ($("#username").val() && $('#password').val()){
