ff73f60de44224c9fe8899615bbd58076128eb86
Evan Cofer <ecofer@trinity.edu>
2016-04-29 18:11:42 -0500

    Added/fixed toolbar which fixed logout and navigation.

--- server/public/javascripts/scripts.js
+++ server/public/javascripts/scripts.js
@@ -1,20 +1,4 @@
 
-/* 
- * Not worth doing this in scalajs and having to worry
- * about some poorly documented or unimplemented 
- * feature, esp. when if we start using OAuth.
- */
-function logout(){
-	$.ajax({
-		url: "/logout",
-		method: "POST",
-		dataType: "json",
-		success:function(data){
-			location.reload();
-		}
-	});
-}
-
 function validate(){
 
 }
